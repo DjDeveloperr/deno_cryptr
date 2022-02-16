59300045d6e3b4b4ab61191cb11819c5385b5e9d
DjDeveloperr <djsandhu2005@gmail.com>
2022-02-16 23:49:56 +0530

    test

--- test.ts
+++ test.ts
@@ -14,4 +14,16 @@ Deno.test("cryptr", async (t) => {
     const decrypted = await cryptr.decrypt(encrypted);
     assertEquals(decrypted, "hello world");
   });
+
+  // Currently does not work because IV length is different
+  // in Deno Cryptr it's 12 because of a limitation in Deno Web Crypto
+  // while in Node it's 16.
+  
+  // await t.step("node cryptr compat", async () => {
+  //   const cryptr = new Cryptr("myTotalySecretKey");
+  //   const decrypted = await cryptr.decrypt(
+  //     "e7b75a472b65bc4a42e7b3f78833a4d00040beba796062bf7c13d9533b149e5ec3784813dc20348fdf248d28a2982df85b83d1109623bce45f08238f6ea9bd9bb5f406427b2a40f969802635b8907a0a57944f2c12f334bd081d5143a357c173a611e1b64a",
+  //   );
+  //   assertEquals(decrypted, "bacon");
+  // });
 });
